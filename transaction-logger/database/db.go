f7b5113ddd4ee363b4a5a02eea0a6d1fb2a5d321
badriaila <ailabadhri@gmail.com>
2025-06-19 10:34:18 -0500

    Updated database connection logic

--- transaction-logger/database/db.go
+++ transaction-logger/database/db.go
@@ -17,7 +17,11 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	_ = godotenv.Load(".env")
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Printf("Warning: .env file not found, using environment variables")
+	}
 
 	dsn := fmt.Sprintf( // Data Source Name (DSN) for PostgreSQL
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
@@ -27,6 +31,8 @@ func ConnectDB() {
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
+
+	log.Printf("Connecting to database with DSN: host=%s user=%s dbname=%s port=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}) // Open a connection to the PostgreSQL database
 
@@ -40,6 +46,4 @@ func ConnectDB() {
 	}
 	DB = db
 	log.Println("Database connection established successfully")
-
-	return
 }
